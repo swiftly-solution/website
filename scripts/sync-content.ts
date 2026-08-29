2f32eebfdfbbb842e68af28cd9185802e0546f9e
Sava Andrei Sebastian <61626661+skuzzis@users.noreply.github.com>
2026-08-29 07:06:53 +0000

    update(sync): Make it show the env

--- scripts/sync-content.ts
+++ scripts/sync-content.ts
@@ -1,29 +1,22 @@
 import { sync, type DocumentRecord } from 'typesense-fumadocs-adapter';
 import { Client } from 'typesense';
 import { readFileSync } from 'node:fs';
-import dotenv from "dotenv";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import "dotenv/config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+console.log(process.env);
 
-dotenv.config({
-  path: path.resolve(__dirname, "../.env"),
-});
+// const filePath = '.next/server/app/static.json.body';
 
-const filePath = '.next/server/app/static.json.body';
+// const content = readFileSync(filePath);
+// const records = JSON.parse(content.toString()) as DocumentRecord[];
 
-const content = readFileSync(filePath);
-const records = JSON.parse(content.toString()) as DocumentRecord[];
+// const client = new Client({
+//     nodes: [{ url: process.env.TYPESENSE_HOST! }],
+//     apiKey: process.env.TYPESENSE_ADMIN_API_KEY!,
+//     connectionTimeoutSeconds: 60 * 15,
+// });
 
-const client = new Client({
-    nodes: [{ url: process.env.TYPESENSE_HOST! }],
-    apiKey: process.env.TYPESENSE_ADMIN_API_KEY!,
-    connectionTimeoutSeconds: 60 * 15,
-});
-
-void sync(client, {
-    typesenseCollectionName: 'swiftlys2',
-    documents: records,
-});
+// void sync(client, {
+//     typesenseCollectionName: 'swiftlys2',
+//     documents: records,
+// });
