cbfa2ede9dfb5620a31b61cf5bf7e8ffc404409d
Sava Andrei Sebastian <61626661+skuzzis@users.noreply.github.com>
2026-08-29 07:16:29 +0000

    update(scripts): Re-enable the uploading

--- scripts/sync-content.ts
+++ scripts/sync-content.ts
@@ -3,20 +3,18 @@ import { Client } from 'typesense';
 import { readFileSync } from 'node:fs';
 import "dotenv/config";
 
-console.log(process.env);
+const filePath = '.next/server/app/static.json.body';
 
-// const filePath = '.next/server/app/static.json.body';
+const content = readFileSync(filePath);
+const records = JSON.parse(content.toString()) as DocumentRecord[];
 
-// const content = readFileSync(filePath);
-// const records = JSON.parse(content.toString()) as DocumentRecord[];
+const client = new Client({
+    nodes: [{ url: process.env.TYPESENSE_HOST! }],
+    apiKey: process.env.TYPESENSE_ADMIN_API_KEY!,
+    connectionTimeoutSeconds: 60 * 15,
+});
 
-// const client = new Client({
-//     nodes: [{ url: process.env.TYPESENSE_HOST! }],
-//     apiKey: process.env.TYPESENSE_ADMIN_API_KEY!,
-//     connectionTimeoutSeconds: 60 * 15,
-// });
-
-// void sync(client, {
-//     typesenseCollectionName: 'swiftlys2',
-//     documents: records,
-// });
+void sync(client, {
+    typesenseCollectionName: 'swiftlys2',
+    documents: records,
+});
